7b0d3c72447bc268fe9c2ad77554e571a6d8b536
Ethan Sattler <mcdasethan2@gmail.com>
2014-11-14 23:59:05 -0500

    added root requirement to run start, stop, and restart

--- daemon/src/tpm-daemon.py
+++ daemon/src/tpm-daemon.py
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 from daemon import Daemon
-import sys, time
+import sys, os, time
 
 PID_FILE = '/var/run/tpm-daemon.pid'
 LOG_FILE = '/var/log/tpm-daemon.log'
@@ -16,27 +16,44 @@ class TPMDaemon(Daemon):
 if __name__ == "__main__":
 	daemon = TPMDaemon(PID_FILE, CWD);
 
+	root = os.geteuid() == 0
+
 	if len(sys.argv) == 2:
 		if 'start' == sys.argv[1]:
+			if not root:
+				print("You must be root to start TPM-Daemon.")
+				sys.exit(1)
+
 			print("Starting TPM-Daemon.")
 			try:
 				daemon.start()
 			except Exception as e:
 				print(e)
+
 		elif 'stop' == sys.argv[1]:
+			if not root:
+				print("You must be root to stop TPM-Daemon.")
+				sys.exit(1)
+			
 			print("Stopping TPM-Daemon.")
 			try:
 				daemon.stop()
-			except:
-				pass
+			except Exception as e:
+				print(e)
+
 		elif 'restart' == sys.argv[1]:
+			if not root:
+				print("You must be root to restart TPM-Daemon.")
+				sys.exit(1)
+					
 			try:
 				print("Stopping TPM-Daemon.");
 				daemon.stop(True)
 				print("Starting TPM-Daemon.")
 				daemon.start()
-			except:
-				pass
+			except Exception as e:
+				print(e)
+
 		elif 'status' == sys.argv[1]:
 			pid = daemon.get_pid();
 			if pid != -1:
@@ -44,9 +61,8 @@ if __name__ == "__main__":
 			else:
 				print("TPM-Daemon is not running")
 		else:
-			print("Invalid command");
+			print("usage: %s start|stop|restart|status" % sys.argv[0])
 			sys.exit(2);
-			sys.exit(0);
 	else:
 		print("usage: %s start|stop|restart|status" % sys.argv[0])
 		sys.exit(2)
