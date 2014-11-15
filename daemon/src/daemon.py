01223c74295a1384f164626f48c41d0c2d7c8959
nilesr <nilesrogoff@gmail.com>
2014-11-14 22:15:19 -0500

    No semicolons. Bad

--- daemon/src/daemon.py
+++ daemon/src/daemon.py
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-import daemon, time
+configfile = "/etc/tpm.conf"
+
+defaultserver = "http:niles.mooo.com:5000"
+
+import daemon, time, os, configparser, BTEdb, urllib.request
 
 def run():
     with daemon.DaemonContext():
-        print("This is a test!");
-
+		config = False
+        if os.path.exists(configfile):
+			config = configparser.ConfigParser(allow_no_value=True)
+			try:
+				config.read(configfile)
+			except:
+				config = False
+		if config:
+			server = config["servers"]["primary"].split(":")
+		else:
+			server = defaultserver.split(":")
+		if not os.path.isdir("/var/cache/tpm"):
+			os.mkdir("/var/cache/tpm")
+		packageindex = BTEdb.Database("/var/cache/tpm/package-index.json")
+		response = urllib.request.urlopen(server[0] + "://" + server[1] + ":" + server[2] + "/package-index.json")
+		packageindex.master = json.loads(response.read())
 if __name__ == "__main__":
-    run();
+    run()
