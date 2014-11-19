a8bd962fb43060b6fb8aeb1325cfde76819b10e4
Ethan Sattler <mcdasethan2@gmail.com>
2014-11-18 23:55:24 -0500

    added more server actions

--- daemon/src/protocol_versions/protocol_1_0.py
+++ daemon/src/protocol_versions/protocol_1_0.py
@@ -12,23 +12,51 @@ class Protocol_1_0(SocketUtils):
 
 		self.methods = {
 			"update": self.update,
-			"get": self.get
+			"get": self.get,
+			"download": self.download,
+			"installed": self.installed,
+			"heartbeat": self.heartbeat,
+			"goodbye": self.goodbye
 		}
 
 	def update(self, args):
-		#Todo: actually update
+		# Todo: actually update
 		self.writeln("UPDATED")
 
 	def get(self, args):
+		# Todo: Actually get
 		if len(args) >= 2:
 			if args[1] == "list":
-				self.writeln("LIST %s" % "/var/blah/list/path")
+				self.writeln("LIST {0}".format("/var/blah/list/path"))
+
+	def download(self, args):
+		# Todo: Actually download
+		if len(args) >= 3:
+			package = args[1]
+			version = args[2]
+ 
+			for i in range(100):
+				self.writeln("STATUS {0} {1} {2}% {3}kb/s {4}kb/s".format(package, version, i, 100, 25, 10))
+
+			self.writeln("DONE {0} {1} /var/cache/tpm/packages/{0}-{1}-x86_64.tpkg".format(package, version))
+
+	def installed(self, args):
+		# Todo: Check args, and if installed
+		self.writeln("FALSE")
+
+	def goodbye(self, args):
+		self.writeln("GOODBYE")
+		self.running = False
+
+	def heartbeat(self, args):
+		self.writeln(self.last_line)
 
 	def run(self):
-		self.writeln("%s" % self.version)
+		self.writeln(self.version)
 
 		while self.running == True:
-			action = shlex.split(self.read_line().lower())
+			self.last_line = self.read_line();
+			action = shlex.split(self.last_line.lower())
 			self.call_method(action)
 
 		self.sock.close();
@@ -40,7 +68,7 @@ class Protocol_1_0(SocketUtils):
 			self.no_such_method()
 
 	def no_such_method(self):
-		self.writeln(b"Error: XXX - No such method")
+		self.writeln("Error: XXX - No such method")
 		self.close()
 
 	def close(self):
