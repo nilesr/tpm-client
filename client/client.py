932618289e69e3b9f71c425b34a8b7adb14c1103
Ethan Sattler <mcdasethan2@gmail.com>
2014-12-23 15:57:14 -0500

    fixed client code

--- client/client.py
+++ client/client.py
@@ -31,12 +31,8 @@ def download():
 
 def install():
 	for package in args.packages:
-		overwrite_line("Determining dependencies for packages: " + package + "..."); # Ethan you misspelled dependencies
-		# Ok Ethan
-		# Functions that start with an "_" are METHODS of a CLASS that you don't want other people using.
-		# Functions that start and end with "__" are RESERVED and should NEVER be used to do something other than OBJECT actions, such as being able to use myObject[index] or str(myObject)
-	write_line("Done\n")
-
+		overwrite_line("Determining dependencies for packages: '" + package + "'.")
+	write_line("\n")
 def remove():
 	pass
 
@@ -46,15 +42,17 @@ def purge():
 def search():
 	pass
 
+actions = {
+	"install": install,
+	"download": download,
+	"remove": remove,
+	"purge": purge,
+	"search": search
+};
+
 if __name__ == '__main__':
-	{
-	  "install": install
-	, "download": download
-	, "remove": remove
-	, "purge": purge
-	, "search": search
-	}.get(args.action,command_not_found)()
-	#print(args)
+	actions.get(args.action,command_not_found)()
+	print(args)
 else:
 	print("Do not execute indirectly. Prepare to die")
 	sys.exit(1)
